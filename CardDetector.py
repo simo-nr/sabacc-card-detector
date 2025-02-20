ac1b26766426698502692e859146d25aa463d753
simonrolly <simonrolly@outlook.com>
2025-02-20 19:01:34 +0100

    record card history to send message after number of same results

--- CardDetector.py
+++ CardDetector.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import VideoStream
-# from Cards import Card
 import Cards
 import time
 
@@ -13,13 +12,21 @@ CARD_THRESH = 30
 CARD_MAX_AREA = 200000
 CARD_MIN_AREA = 5000
 
+CARD_HISTORY = 5
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-video_path = "media/test_vid_4.mov"
+global video_path
+video_path = "media/test_vid_2.mov"
+global videostream
 videostream = VideoStream.VideoStream(video_path).start()
 
 
 def main():
+    detected_cards = [set() for _ in range(CARD_HISTORY)] # Keep track of cards detected in previous frames
+    last_message_send = []
+    frame_counter = 0
+
     cam_quit = 0 # Loop control variable
     while cam_quit == 0:
         # Grab frame from video stream
@@ -41,26 +48,52 @@ def main():
         k = 0
         if len(cnts_sort) != 0:
 
+            detected_cards.append(set())
+
             for i in range(len(cnts_sort)):
                 if cnt_is_card[i] == 1:
                     cards.append(Cards.preprocess_card(cnts_sort[i], frame))
                     frame = draw_results(frame, cards[k])
+
+                    # append cards in last frame with info of card
+                    detected_cards[-1].add((cards[k].sign, cards[k].rank, cards[k].suit))
+
                     k = k + 1
             
+            # draw all the card contours together
             if len(cards) != 0:
                 tmp_cnts = []
                 for i, card in enumerate(cards):
                     # cv2.imshow(f"Card: {i}", card.debug_view)
+                    # cv2.imshow(f"Card: {i}", card.warp)
                     tmp_cnts.append(card.contour)
                 cv2.drawContours(frame, tmp_cnts, -1, (255,0,0), 2)
+        
+        # check list of previous frames
+        if len(detected_cards) > CARD_HISTORY:
+            detected_cards.pop(0)
 
+        # check if all items in detected_cards are the same
+        if all(s == detected_cards[0] for s in detected_cards) and detected_cards[0] != set():
+            # print(f"Cards are the same in the last {len(detected_cards)} frames: frame {frame_counter}")
+            if last_message_send != detected_cards[-1]:
+                last_message_send = detected_cards[-1]
+                print("send message: ", last_message_send)
+
+            # debug stuff
+            # if detected_cards[0] != {('Negative', '2', 'Square'), ('Positive', '3', 'Circle')}:
+            #     print(detected_cards[0])
+        
         # Show the video feed
         cv2.imshow("Live Feed", frame)
 
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             cam_quit = 1
-
+        
+        frame_counter += 1
+        # time.sleep(0.25)
+        
     cv2.destroyAllWindows()
     videostream.stop()
 
@@ -87,7 +120,7 @@ def find_cards(frame):
     # Initialize empty sorted contour and hierarchy lists
     cnts_sort = []
     hier_sort = []
-    cnt_is_card = np.zeros(len(contours),dtype=int)
+    cnt_is_card = np.zeros(len(contours), dtype=int)
 
     # Fill empty lists with sorted contour and sorted hierarchy.
     # Now, the indices of the contour list still correspond with those of the hierarchy list. 
@@ -136,4 +169,7 @@ def draw_results(frame, card):
     return frame
 
 if __name__ == "__main__":
+    # for i in range(4):
+    #     video_path = f"media/test_vid_{i+1}.mov"
+    #     videostream = VideoStream.VideoStream(video_path).start()
     main()
